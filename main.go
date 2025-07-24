6327d1471ec1b0ff9fe9d8b4c057985bb2217fbc
Amir228Kali <123566gn@gmail.com>
2025-07-24 06:15:09 +0300

    modified:   main.go

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"github.com/Amir228Kali/simplebank/api"
 	db "github.com/Amir228Kali/simplebank/db/sqlc"
 	"github.com/Amir228Kali/simplebank/util"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/golang-migrate/migrate/v4/source/github"
 	_ "github.com/lib/pq"
 )
 
@@ -23,6 +27,7 @@ func main() {
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
+	runDBMigration(config.MIGRATION_URL, config.DBSource)
 
 	store := db.NewStore(conn)
 	server, err := api.NewServer(config, store)
@@ -33,4 +38,15 @@ func main() {
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create migration:", err)
+	}
+	if err := migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migration:", err)
+	}
+	log.Println("DB migration completed successfully")
 }
